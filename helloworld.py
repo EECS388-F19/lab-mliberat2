642801d88e87c67f1f0d5cbdd74a28dc5c26f43b
libercm <libercm@umich.edu>
2019-09-09 15:29:11 -0400

    added first stab of helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from numpy import random
+
+if __name__ == '__main__':
+    print('Michael')
+    x = random.randint(1,101)
+    y = random.randint(1,101)
+    print(x)
+    print(y)
+    print('Sum = ' + x+y)
+    print('Average = ' + (x+y))
